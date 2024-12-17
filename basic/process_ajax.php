8074870a722766f56dd16c99fb4a4b664521c5b6
sowbaraniga26 <sowbaraniga26@gmail.com>
2024-12-17 19:53:44 +0530

    Request with Array Response

---
+++ basic/process_ajax.php
@@ -0,0 +1,21 @@
+<?php
+
+    echo "This is the PHP page<br>";
+
+    $names[] = "Anu";
+    $names[] = "Raja";
+    $names[] = "Ragav";
+    $names[] = "Roja";
+
+    $c = 1;
+
+    foreach ($names as $name)
+    
+    {
+        echo $c . " " . $name . "<br>";
+
+        $c++;
+
+    }
+    
+?>
