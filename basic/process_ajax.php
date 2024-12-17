05ca88d2bd0c47cc1c6265e770b02a0ecb6f287d
sowbaraniga26 <sowbaraniga26@gmail.com>
2024-12-17 20:01:19 +0530

    Update process_ajax.php

--- basic/process_ajax.php
+++ basic/process_ajax.php
@@ -1,5 +1,21 @@
 <?php
 
-    echo "This is the PHP page";
-    
+  echo "This is the PHP page<br>";
+
+  $names[] = "Anu";
+  $names[] = "Raja";
+  $names[] = "Ragav";
+  $names[] = "Roja";
+
+  $c = 1;
+
+ foreach ($names as $name)
+
+ {
+    echo $c . " " . $name . "<br>";
+
+    $c++;
+
+  }
+
 ?>
