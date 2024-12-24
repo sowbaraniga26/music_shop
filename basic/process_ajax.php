67488a55c7e35ac51c74cb65db71b3aab06607b8
sowbaraniga26 <sowbaraniga26@gmail.com>
2024-12-24 19:38:19 +0530

    Request with Query response

--- basic/process_ajax.php
+++ basic/process_ajax.php
@@ -12,10 +12,28 @@
  foreach ($names as $name)
 
  {
-    echo $c . " " . $name . "<br>";
+     if ($_REQUEST['var1'] == $name)
+     {
+        echo $c ." " .$name . " is the important name<br><br>";
 
+     }
+     else {
+        echo $c . " " . $name . "<br>";
+     }
     $c++;
-
-  }
+ }
+ if (isset($_REQUEST ['var2']))
+{
+    if ($_REQUEST['var2'] == '')
+    {
+        echo "We have an empty variable so we&apos;re unable to show you the result.";
+    }
+    else {
+        echo "We have some " .$_REQUEST ['var2'].". We will eat them";
+    }
+}
+else {
+   echo "Note: Something is more there but not visible because of a variable inside it, which is actually not declared any where.";
+}
 
 ?>
